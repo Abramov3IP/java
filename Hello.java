16458a46da108bb58dfb8822343fa6526cc3ccf7
Abramov3IP <72501957+Abramov3IP@users.noreply.github.com>
2020-10-09 10:37:25 +0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+	public static void main (String[] args)
+	{
+		int x = 2;
+		int y = 2;
+		System.out.println((int)Math.pow(2, -x) - Math.cos(x) + Math.sin(2*x*y));
+		System.out.println((int)2 * 1 / Math.tan(3 * x) - 1 / (12 * Math.pow(x, 2) + 7 * x - 5));
+	}
+}
