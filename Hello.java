094dde330dfa2879c95c6069c0096b5500941bff
Abramov3IP <72501957+Abramov3IP@users.noreply.github.com>
2020-10-13 14:20:02 +0300

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Hello
 {
 	public static void main (String[] args)
@@ -8,3 +10,65 @@ class Hello
 		System.out.println((int)2 * 1 / Math.tan(3 * x) - 1 / (12 * Math.pow(x, 2) + 7 * x - 5));
 	}
 }
+
+class circle
+{
+	public static void main (String[] args)
+	{
+		double a, b, c, radius, S, p;
+		a = 18;
+		b = 24;
+		c = 30;
+		p = (a + b + c) / 2;
+		S = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		radius = S / p;
+		S = radius * radius;
+		System.out.println((float) S);
+	}
+}
+
+class rectangle
+{
+	public static void main (String[] args)
+	{
+		double a, b, c, d;
+		a = 4;
+		b = 2;
+		c = 6;
+		d = 2;
+		if (a / b == c / d)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	}
+}
+
+class coordinates
+{
+	public static void main (String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int x, y;
+		System.out.println("Enter x:");
+		x = in.nextInt();
+		System.out.println("Enter y:");
+		y = in.nextInt();
+		if (x < 5  && x > -5 && y < 5 && y > 0 && x * x + y * y < 18)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	}
+}
+
+
+
+
+
